ee222f7d2144c0acdb0df870be0748e35b8f24d9
Meroua Hadj Benaichouche <marouam312@gmail.com>
2025-01-14 04:58:36 +0100

    getuser if cookieSet true

--- src/hooks/useHeader.ts
+++ src/hooks/useHeader.ts
@@ -1,48 +1,21 @@
-import  Cookies  from 'js-cookie';
-import { authLogin, actAuthLogout } from "@/store/auth/authSlice";
+import {  actAuthLogout, actGetUser } from "@/store/auth/authSlice";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { useSearchParams } from 'react-router-dom';
-import axios from "@/services/api/axios.config";
-import getCookie from '@/utils/getCookie';
 
 const useHeader = () => {
     const dispatch = useAppDispatch();
   const [searchParams, setSearchParams] = useSearchParams();
   useEffect(() => {
-    const CheckCookies = async() => {
-    
-        const accessToken = getCookie("accessToken");
-
-        let user;
-        try {
-          user = JSON.parse(getCookie("user") as string);
-          console.log(user, accessToken);
-        } catch (error) {
-          console.log(error);
-          return;
-        }
-        if (accessToken && user) {
-          dispatch(authLogin({ accessToken, user }));
-          Cookies.remove("accessToken");
-          Cookies.remove("user");
-        }
-   
-    }
+  
     const cookieSet = searchParams.get('cookieSet')
     
     if (cookieSet === "true") {
-      const setCookies = async () => {
-        await axios.get('/auth/set-cookie')
-        
-       
-        setSearchParams({});
-      }
-      setCookies()
+                dispatch(actGetUser());
+
 
     }
-      CheckCookies();
   }, [searchParams, setSearchParams, dispatch]);
   const { error, loading, accessToken, user } = useAppSelector(
     (state) => state.auth
